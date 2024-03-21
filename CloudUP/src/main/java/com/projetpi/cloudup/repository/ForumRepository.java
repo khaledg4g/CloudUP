3879b74768380f7cd74b0ecd2c38860c37e989c4
salmaks <salma.ksantini@esprit.tn>
2024-03-21 16:16:32 +0100

    suppression cloud up

--- CloudUP/src/main/java/com/projetpi/cloudup/repository/ForumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projetpi.cloudup.repository;
-
-import com.projetpi.cloudup.entities.Forum;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForumRepository extends JpaRepository<Forum,Long> {
-}
