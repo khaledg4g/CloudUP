5f5db8c6ba6b683d831043fcaaa9bfb356cd7df2
Salah <salah.elabed@esprit.tn>
2024-03-29 03:52:55 +0100

    add origins

--- src/main/java/com/projetpi/cloudup/RestController/ReclamationController.java
+++ src/main/java/com/projetpi/cloudup/RestController/ReclamationController.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 public class ReclamationController {
